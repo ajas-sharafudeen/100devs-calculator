3eba4c168abed4bfcb6768037b61ed485e2bb57f
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-05-22 18:04:30 +0530

    added pseudocode

---
+++ main.js
@@ -0,0 +1,11 @@
+//accept user inputs of number, operator, and number
+//should accept decimel numbers
+//store inputs
+//recognize inputs and perform calculations
+//result
+
+//additional features
+//accept longer arithmetic operations
+//display all input as it is being entered
+//store previous total as start of next operation
+//clear button should clear all entries
